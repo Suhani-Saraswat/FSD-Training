65697a9460d4bc50e44122d7a034c6db28c0e29c
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-01-28 16:25:22 +0530

    script

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+console.log(3+4);
+
+console.error("j");
+
+console.warn("k");
+
+// let a=1;
+// let b;
+// a=b;
+
+// var c=1;
+// var d;
+// c=d;
+
+// const e=1;
+// const f;
+// e=f;
