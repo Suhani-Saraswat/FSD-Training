d800dce43ea93d3fa284518fbfdd9eaf29432424
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-04 15:21:51 +0530

    typeof

--- script.js
+++ script.js
@@ -4,17 +4,3 @@ console.log(3+4);
 console.error("j");
 
 console.warn("k");
-
-// let a=1;
-// let b;
-// a=b;
-
-// var c=1;
-// var d;
-// c=d;
-
-// const e=1;
-// const f;
-// e=f;
-
-console.log(9+3);
