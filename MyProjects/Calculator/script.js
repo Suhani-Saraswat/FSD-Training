dc6a243ae02583ce8f0a87b5896d8d0b319d850b
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-22 21:32:28 +0530

    Script JS

--- MyProjects/Calculator/script.js
+++ MyProjects/Calculator/script.js
@@ -22,3 +22,32 @@ button_input.forEach((button_class) => {
     input.value += button_class.value;
   });
 });
+
+//Solve the user's input when clicked on equal sign
+equal.addEventListener("click", () => {
+  equal_pressed = 1;
+  let inp_val = input.value;
+  try {
+    //evaluate user's input
+    let solution = eval(inp_val);
+    //True for natural numbers
+    //false for decimals
+    if (Number.isInteger(solution)) {
+      input.value = solution;
+    } else {
+      input.value = solution.toFixed(2);
+    }
+  } catch (err) {
+    //If user has entered invalid input
+    alert("Invalid Input");
+  }
+});
+
+//Clear Whole Input
+clear.addEventListener("click", () => {
+  input.value = "";
+});
+//Erase Single Digit
+erase.addEventListener("click", () => {
+  input.value = input.value.substr(0, input.value.length - 1);
+});
