a07723277818158595d64d8f9d7f11fd435ec6b8
Suhani-Saraswat <suhanisaraswat1795@gmail.com>
2026-02-22 21:32:09 +0530

    Script JS

---
+++ MyProjects/Calculator/script.js
@@ -0,0 +1,24 @@
+let equal_pressed = 0;
+//Refer all buttons excluding AC and DEL
+let button_input = document.querySelectorAll(".input-button");
+//Refer input,equal,clear and erase
+let input = document.getElementById("input");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("erase");
+
+window.onload = () => {
+  input.value = "";
+};
+
+//Access each class using forEach
+button_input.forEach((button_class) => {
+  button_class.addEventListener("click", () => {
+    if (equal_pressed == 1) {
+      input.value = "";
+      equal_pressed = 0;
+    }
+    //display value of each button
+    input.value += button_class.value;
+  });
+});
